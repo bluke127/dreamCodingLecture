58b6f85459094fe21bee79b388ef44bbe0c7470d
blukoo <dnehdrkd123@naver.com>
2023-05-13 22:36:48 +0900

    [feat]react
    pracice 프로젝트 추가

---
+++ react/practice/src/index.tsx
@@ -0,0 +1,38 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import NotFounds from "@pages/NotFounds";
+import TestIndex from "@pages/TestPages/TestIndex";
+import TestPage1 from "./pages/TestPages/TestPage1";
+import TestPage2 from "./pages/TestPages/TestPage2";
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App></App>,
+    errorElement: <NotFounds />,
+    children: [
+      {
+        index: true,
+        element: <TestIndex />,
+      },
+      {
+        index: true,
+        element: <TestPage1 />,
+      },
+      {
+        index: true,
+        element: <TestPage2 />,
+      },
+    ],
+  },
+]);
+const root = ReactDOM.createRoot(
+  document.getElementById("root") as HTMLElement
+);
+root.render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
