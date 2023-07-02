588668566180677db3b785a3bbd49a7697937750
blukoo <dnehdrkd123@naver.com>
2023-07-02 20:58:36 +0900

    [fix]드위터 게시물

--- nodeJsLecture/12-server-template/router/tweets.js
+++ nodeJsLecture/12-server-template/router/tweets.js
@@ -1,7 +1,15 @@
 import express from "express";
 import "express-async-errors";
 import * as tweetController from "../controller/tweet.js";
+import { validate } from "../middleware/validator.js";
+import { body } from "express-validator";
 const router = express.Router();
+const validateTweet = [
+  body("text")
+    .trim()
+    .isLength({ min: 3 })
+    .withMessage("text should be at least 3 characters"),
+];
 let tweets = [
   {
     id: "1",
@@ -17,9 +25,9 @@ router.get("/", tweetController.getTweets);
 //GET /tweets/:id
 router.get("/:id", tweetController.getTweet);
 //POST /tweets
-router.post("/", tweetController.createTweet);
+router.post("/", [validateTweet, validate], tweetController.createTweet);
 //PUT /tweets/:id
-router.put("/:id", tweetController.updateTweet);
+router.put("/:id", [validateTweet, validate], tweetController.updateTweet);
 //DELETE /tweets/:id
 router.delete("/:id", tweetController.deleteTweet);
 export default router;
