c75f103cae764030e681a8f3a8149aca72b067ed
blukoo <dnehdrkd123@naver.com>
2023-07-10 15:29:26 +0900

    [feat]소켓

--- nodeJsLecture/12-server-template/app.js
+++ nodeJsLecture/12-server-template/app.js
@@ -5,6 +5,7 @@ import helmet from "helmet";
 import "express-async-errors";
 import tweetsRoute from "./router/tweets.js";
 import authRoute from "./router/auth.js";
+import { Server } from "socket.io";
 const app = express();
 app.use(express.json());
 app.use(helmet());
@@ -12,6 +13,7 @@ app.use(cors());
 app.use(morgan("tiny"));
 app.use("/tweets", tweetsRoute);
 app.use("/auth", authRoute);
+
 app.use((req, res, next) => {
   res.sendStatus(404);
 });
@@ -19,4 +21,15 @@ app.use((error, req, res, next) => {
   console.error(error);
   res.sendStatus(500);
 });
-app.listen(8081);
+console.log(process.env.JWT_EXPIRES_SEC);
+const server = app.listen(8081);
+const socketIO = new Server(server, {
+  cors: {
+    origin: "*",
+  },
+});
+socketIO.on("connection", (socket) => {
+  console.log("client is here");
+  socketIO.emit("dwitter", "hello하트");
+  socketIO.emit("dwitter", "hello하트ㄴㄴ");
+});
