7faa1beac92b94dcec15849175b5bd74a3ffac7f
blukoo <dnehdrkd123@naver.com>
2023-09-25 23:26:13 +0900

    [feat]구글로그인
    next

--- nextjs/Project/instagram/src/pages/api/auth/[...nextauth].ts
+++ nextjs/Project/instagram/src/pages/api/auth/[...nextauth].ts
@@ -1,6 +1,7 @@
-import NextAuth from "next-auth";
+import { addUser } from "@/service/user";
+import NextAuth, { NextAuthOptions } from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
-export const authOptions = {
+export const authOptions: NextAuthOptions = {
   // Configure one or more authentication providers
   providers: [
     GoogleProvider({
@@ -17,13 +18,30 @@ export const authOptions = {
     newUser: "/auth/new-user", // New users will be directed here on first sign in (leave the property out if not of interest)
   },
   callbacks: {
-    async session({ session, token, user }) {
-      // Send properties to the client, like an access_token and user id from a provider.
-      session.accessToken = token.accessToken
-      session.user.id = token.id
-      
-      return session
-    }
-  }
+    async signIn({ user: { id, name, image, email } }) {
+      if (!email) {
+        return false;
+      }
+      addUser({
+        id,
+        name: name || "",
+        image,
+        email,
+        username: email.split("@")[0],
+      });
+      return true;
+    },
+    async session({ session }) {
+      console.log(session);
+      const user = session?.user;
+      if (user) {
+        session.user = {
+          ...user,
+          username: user.email?.split("@")[0] || "",
+        };
+      }
+      return session;
+    },
+  },
 };
 export default NextAuth(authOptions);
